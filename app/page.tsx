804fd9ba8078b5562258a396249aa4bfbf8315c8
[dyad] <teuzintw@gmail.com>
2026-03-01 08:23:11 -0300

    Updated app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -93,7 +93,7 @@ export default function Home() {
             }}
           />
         ))}
-        <div className="absolute top-4 right-4 z-50 cursor-pointer">
+        <div className="absolute top-4 right-4 z-50">
           <LoginSwitchTheme />
         </div>
       </div>
