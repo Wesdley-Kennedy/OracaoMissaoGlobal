fcba0d0fc3ae0dc2ba3a6bf6e6e92393e33fc485
matheus <matheussfilipej@gmail.com>
2026-03-01 19:03:29 -0300

    att

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
-import type { NextConfig } from "next";
+import { type NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverExternalPackages: ["@prisma/client"],
-  },
+  // experimental: { serverExternalPackages: ["@prisma/client"] }, // Remover
 };
 
 export default nextConfig;
